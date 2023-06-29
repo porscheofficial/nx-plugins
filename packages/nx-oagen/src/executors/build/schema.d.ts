956a76fc516dc15c1805d68a196f9e65fcbd8287
Johann Wagner <johann.wagner.extern@porsche.digital>
2023-06-29 14:28:50 +0200

    remove oagen

--- packages/nx-oagen/src/executors/build/schema.d.ts
+++
@@ -1,8 +0,0 @@
-import { Generator } from "../../types"
-
-export interface BuildExecutorSchema {
-    specFile: string
-    outDir: string
-    generator: Generator
-    additionalProperties: Record<string, string | number | boolean>
-} // eslint-disable-line
--- packages/nx-oagen/src/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as libraryGenerator } from "./generators/library/generator"
-export { default as codegenExecutor } from "./executors/build/executor"
